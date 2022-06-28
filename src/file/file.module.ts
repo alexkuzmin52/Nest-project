7bf684f3e3c1bced05c3c489f74dd1d73aa2d00f
alexkuzmin52 <kuzminalex687.gmail.com>
2022-06-29 01:42:18 +0300

    publish to the GitHub

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
-// import { AuthModule } from '../auth/auth.module';
 import { FileService } from './file.service';
 import { photoSchema } from './schemas/photo-schema';
 
 @Module({
   imports: [
-    // AuthModule,
     MongooseModule.forFeature([{ name: 'photo', schema: photoSchema }]),
   ],
   providers: [FileService],
